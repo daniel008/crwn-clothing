e971d6b020d6dd52845c4d0e3e9575d2589a4a73
DanielMa <mwldaniel_008@126.com>
2022-05-24 10:17:16 +0800

    build the cart icon & dropdown component.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ReactComponent as CrwnLog } from '../../assets/crown.svg'
 import { UserContext } from '../../contexts/user.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
@@ -27,7 +29,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
