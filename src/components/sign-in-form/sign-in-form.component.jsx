d6b92f873a7f4f530e616692983faf9917e83ded
DanielMa <mwldaniel_008@126.com>
2022-06-05 12:32:18 +0800

    converted sign-in-form && sign-up-form scss files into styled-components.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -4,8 +4,8 @@ import {
   signInAuthUserWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
-import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component'
-import './sign-in-form.styles.scss'
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component'
+import { SignInContainer, ButtonContainer } from './sign-in-form.styles.jsx'
 
 const defaultFormFields = {
   email: '',
@@ -28,7 +28,7 @@ const SignInForm = () => {
     event.preventDefault()
 
     try {
-      const { user } = await signInAuthUserWithEmailAndPassword(email, password)
+      await signInAuthUserWithEmailAndPassword(email, password)
 
       // console.log(user)
       resetFormFields()
@@ -52,7 +52,7 @@ const SignInForm = () => {
   }
 
   return (
-    <div className="sign-up-container">
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -73,14 +73,18 @@ const SignInForm = () => {
           onChange={handleChange}
           value={password}
         />
-        <div className="buttons-container">
+        <ButtonContainer>
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>
+          <Button
+            type="button"
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            onClick={signInWithGoogle}
+          >
             Google sign in
           </Button>
-        </div>
+        </ButtonContainer>
       </form>
-    </div>
+    </SignInContainer>
   )
 }
 
