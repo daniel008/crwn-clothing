272f4fc0b1b9248b950fb250b404d9a1d0513dbc
DanielMa <mwldaniel_008@126.com>
2022-06-08 07:33:52 +0800

    create && set up userSelector.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import { CartContext } from '../../contexts/cart.context'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '../../store/user/user.selector'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ReactComponent as CrwnLog } from '../../assets/crown.svg'
-import { UserContext } from '../../contexts/user.context'
-import { CartContext } from '../../contexts/cart.context'
+// import { UserContext } from '../../contexts/user.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import {
   NavigationContainer,
@@ -14,7 +16,8 @@ import {
 } from './navigation.styles'
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
+  // const { currentUser } = useContext(UserContext)
+  const currentUser = useSelector(selectCurrentUser)
   const { isCartOpen } = useContext(CartContext)
   // console.log(currentUser)
   return (
