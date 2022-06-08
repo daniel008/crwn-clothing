6b397a89fcb5ca7e671a8f85fed516c215fee757
DanielMa <mwldaniel_008@126.com>
2022-06-08 16:56:23 +0800

    migrating userContext && categoriesContext to Redux.

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,5 +1,5 @@
 import { createAction } from '../../utils/reducer/reducer.utils'
-import { USER_ACTION_TYPES } from './user.type'
+import { USER_ACTION_TYPES } from './user.types'
 
 export const setCurrentUser = (user) =>
   createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
