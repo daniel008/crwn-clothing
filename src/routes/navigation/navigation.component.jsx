3c5ffdf81322518ef7cd15f78c1ed30454be1042
DanielMa <mwldaniel_008@126.com>
2022-07-25 18:55:57 +0800

    converted into sign-out saga.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { selectIsCartOpen } from '../../store/cart/cart.selector'
 import { selectCurrentUser } from '../../store/user/user.selector'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ReactComponent as CrwnLog } from '../../assets/crown.svg'
-import { signOutUser } from '../../utils/firebase/firebase.utils'
+// import { signOutUser } from '../../utils/firebase/firebase.utils'
+import {signOutStart} from '../../store/user/user.action.js'
 import {
   NavigationContainer,
   NavLinks,
@@ -15,8 +16,11 @@ import {
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser)
-  const isCartOpen = useSelector(selectIsCartOpen)
   // console.log(currentUser)
+  const isCartOpen = useSelector(selectIsCartOpen)
+  const dispatch = useDispatch()
+  const signOutUser = () => dispatch(signOutStart())
+  
   return (
     <>
       <NavigationContainer>
--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -20,3 +20,9 @@ export const signUpStart = (email, password, displayName) => createAction(USER_A
 export const signUpSuccess = (user, additionalInformation) => createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, {user, additionalInformation})
 
 export const signUpFailed = (error) => createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error)
+
+export const signOutStart = () => createAction(USER_ACTION_TYPES.SIGN_OUT_START)
+
+export const signOutSuccess = () => createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS)
+
+export const signOutFailed = (error) => createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error)
